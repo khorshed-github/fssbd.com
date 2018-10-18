6b0cfcd71b586190532a6e8f91a9e0a6ac6c28ab
khorshed Alam <mdkhorsheda15@gmail.com>
2018-10-18 08:49:52 +0600

    self-employments workplace works

---
+++ workplace/index.php
@@ -0,0 +1,205 @@
+<!DOCTYPE html>
+<html lang="en-US">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="content-type" content="text/html;charset=UTF-8" />
+    <link rel="profile" />
+    <title>Aim Forever &#8211; Don&#039;t be aimless, target your best aim</title>
+
+    <link rel="shortcut icon" href="images/favicon.ico" title="Favicon" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="css/bootstrap.css" />
+    <link rel="stylesheet" href="font-awesome-4.7.0/css/font-awesome.min.css" />
+</head>
+
+<body style="background:url(images/patterns/body-bg7.png);repeat-x;">
+    <div class="container" style="background: #fafafa;">
+        <div class="row">
+            <div class="col-sm-12">
+                <nav class="navbar navbar-default" role="navigation">
+                    <!-- Brand and toggle get grouped for better mobile display -->
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse1">
+                            <span class="sr-only">Toggle navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                        <a class="navbar-brand" href="#"><span class="today-date">Thursday , October 11 2018</span> </a>
+                    </div>
+
+                    <!-- Collect the nav links, forms, and other content for toggling -->
+                    <div class="collapse navbar-collapse navbar-ex1-collapse1">
+                        <ul class="nav navbar-nav">
+                            <li class="active"><a href="#">About Us</a></li>
+                            <li><a href="#">Contact Us</a></li>
+                            <li><a href="#">TOS</a></li>
+                            <li><a href="#">Privacy Policy</a></li>
+                        </ul>
+                        <form class="navbar-form navbar-right" role="search">
+                            <div class="form-group">
+                                <input type="text" class="form-control" placeholder="Search">
+                            </div>
+                            <button class="btn btn-default" type="submit" value="Search"><i class="fa fa-search"></i></button>
+                        </form>
+
+                    </div><!-- /.navbar-collapse -->
+                </nav>
+            </div>
+        </div>
+
+
+
+        <div class="row">
+            <div class="col-sm-3">
+                <div class="page-header">
+                    <h1>Aim Forever</h1>
+                    <p><span>Don&#039;t be aimless, target your best aim</span></p>
+                </div>
+
+            </div>
+            <div class="col-sm-9">
+                <img class="img-responsive img-thumbnail" src="uploads/unnamed.png" alt="Chania">
+
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-sm-12">
+                <nav class="navbar navbar-default" role="navigation">
+                    <!-- Brand and toggle get grouped for better mobile display -->
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse2">
+                            <span class="sr-only">Toggle navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                        <a class="navbar-brand" href="#">Title</a>
+                    </div>
+
+                    <!-- Collect the nav links, forms, and other content for toggling -->
+                    <div class="collapse navbar-collapse navbar-ex1-collapse2">
+                        <ul class="nav navbar-nav">
+                            <li class="active"><a href="#">Home</a></li>
+                            <li><a href="#">Business</a></li>
+                            <li><a href="#">Career</a></li>
+                            <li><a href="#">Technology</a></li>
+                            <li><a href="#">Finance</a></li>
+                            <li><a href="#">Legal</a></li>
+                            <li><a href="#">Marketing</a></li>
+                        </ul>
+                        <form class="navbar-form navbar-right" role="search">
+                            <button class="btn btn-default" type="submit" value="Search"><i class="fa fa-search"></i></button>
+                        </form>
+                    </div><!-- /.navbar-collapse -->
+                </nav>
+            </div>
+        </div>
+
+        <!--/* // Main Body Section. */ -->
+
+        <div class="main-body">
+            <div class="row">
+                <div class="col-sm-12">
+                    <div class="row">
+                        <div class="col-sm-8">
+
+                            <ol class="breadcrumb">
+                                <li>
+                                    <a href="#">[Home]</a>
+                                </li>
+                                <li>
+                                    <a href="#">[About]</a>
+                                </li>}
+                                <li class="active">[Contact]</li>
+                            </ol>
+                            <!--/* // Main Contents Section. */ -->
+                           <div class="row">
+                                <div class="col-sm-12">
+                                    <img class="img-responsive img-thumbnail" src="uploads/unnamed.png" width="100%" alt="Chania">
+                                </div>
+                            </div>
+
+                            <div class="content">
+                                <h1>Remote Workforce</h1>
+                                <p><i class="fa fa-user"></i>&nbsp;<a href="">User Name</a> <i class="fa fa-clock-o"></i>&nbsp;September 12, 2018</p>
+                                <p>
+                                    I have a preference for my team to be onsite, but for some functions and aspects of our discharge faithfulness, these elements are outsourced. Currently 40% benefit of American workers are vibes pain some organization remotely, without having to be assert at their place of concern at all times. This trend creates demand for conveniently training <img class="img-responsive img-thumbnail pull-right" src="uploads/cox_chart-310x165.jpg" alt="Chania">unapproachable employees through video conferencing platforms, especially for social media and programming positions. All that is required is a computer and Internet
+                                    I have a preference for my team to be onsite, but for some functions and aspects of our discharge faithfulness, these elements are outsourced. Currently 40% benefit of American workers are vibes pain some organization remotely, without having to be assert at their
+                                     place of concern at all times. This trend creates demand for conveniently training unapproachable employees through video conferencing platforms, especially for social media and programming positions. All that is required is a computer and Internet
+                                </p>
+                                <p>
+                                    <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit quod quia necessitatibus vitae, ducimus labore corporis dolore expedita, neque accusantium optio, voluptatem quasi. Minima, consequatur, quas! Temporibus voluptates ut a!</span>
+                                    <span>Necessitatibus officiis sequi excepturi rem eveniet culpa optio sed corporis deleniti, iure consequuntur eaque dolor cum porro odio incidunt fuga expedita earum id,     <img class="img-responsive img-thumbnail pull-left" src="uploads/Business-Etiquette-Training-310x165.jpg" alt="Chania">voluptas illo error. Aspernatur molestias totam repellendus.</span>
+                                    <span>Magnam explicabo, ipsam. Dignissimos, magnam, molestiae eveniet maiores animi eius ipsum vero cupiditate quia, sequi quae hic nostrum corporis commodi illo debitis? Doloribus quas illum, ipsa nobis nam voluptates ad.</span>
+                                    <span>Alias perspiciatis consequuntur blanditiis similique expedita omnis, iste officiis iure sint modi eaque eos corporis repudiandae ex doloribus ratione neque voluptas. Illo porro quis unde illum, consequatur. Ipsa, nisi, eligendi!</span>
+                                <span>Quae laboriosam nesciunt fugiat quisquam consectetur eveniet suscipit deleniti similique vero architecto. Perferendis ipsam consequuntur eius quaerat, aliquam quibusdam sunt autem, sint labore illum veniam explicabo inventore cum nisi hic.</span>
+                                </p>
+                            </div>
+                             <div class="row">
+                                <div class="col-sm-12">
+                                    <img class="img-responsive img-thumbnail" src="uploads/unnamed.png" width="100%" alt="Chania">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-4">
+
+                            <div class="row">
+                                <div class="col-sm-12">
+                                    <h3 style="background: url(images/stripe.png);">Sponsorse</h3>
+                                    <img class="img-responsive img-thumbnail" src="uploads/Business-Etiquette-Training-660x330.jpg" alt="Chania">
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-sm-12">
+                                    <h3 style="background: url(images/stripe.png);">Recent Post</h3>
+                                    <?php
+                                       for ($x = 0; $x <= 5; $x++) {
+                                            ?>
+                                    <p>
+                                        <a href="">Advanced Mobile SEO TIPS For Mobile Marketing</a>
+                                        <br>
+                                        <i class="fa fa-clock-o"></i>&nbsp;September 12, 2018
+                                    </p>
+                                    <?php
+                                       }
+                                    ?>
+                                </div>
+                            </div>
+
+                            <div class="row">
+                                <div class="col-sm-12">
+                                    <img class="img-responsive img-thumbnail" src="uploads/cox_chart-310x165.jpg" width="100%" alt="Chania">
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-sm-12">
+                                    <img class="img-responsive img-thumbnail" src="uploads/Business-Etiquette-Training-660x330.jpg" width="100%" alt="Chania">
+                                </div>
+                            </div>
+                           <div class="row">
+                                <div class="col-sm-12">
+                                    <img class="img-responsive img-thumbnail" src="uploads/cox_chart-310x165.jpg" width="100%" alt="Chania">
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-sm-12">
+                                    <img class="img-responsive img-thumbnail" src="uploads/Business-Etiquette-Training-660x330.jpg" width="100%" alt="Chania">
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <script type="text/javascript" src="js/jquery.min.js"></script>
+    <script type="text/javascript" src="js/bootstrap.js"></script>
+</body>
+
+</html>
